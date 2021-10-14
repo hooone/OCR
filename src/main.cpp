992292b8148aec5e21502c45d91687047a551201
hooone <331474547@qq.com>
2021-10-14 14:27:27 +0800

    引用gflas

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
+#include <gflags/gflags.h>
 
 using namespace std;
 
+DEFINE_string(image_dir, "", "Dir of input image.");
 int main(int argc, char **argv)
 {
     std::cout << "hello world!" << endl;
+
+    // 解析输入的参数
+    google::ParseCommandLineFlags(&argc, &argv, true);
+    std::cout << "image_dir:"
+              << FLAGS_image_dir << endl;
 }
